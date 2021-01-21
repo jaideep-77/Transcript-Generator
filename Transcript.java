fc2657d33c22d22cba1919ba6883f81e553747b6
jaideep-77 <77744276+jaideep-77@users.noreply.github.com>
2021-01-21 03:18:02 -0500

    Update Transcript.java

--- Transcript.java
+++ Transcript.java
@@ -1,4 +1,3 @@
-package Assignment2;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -6,27 +5,6 @@ import java.util.Objects;
 import java.util.Scanner;
 import java.util.Set;
 import java.io.*;
-
-
-/* PLEASE DO NOT MODIFY A SINGLE STATEMENT IN THE TEXT BELOW.
-READ THE FOLLOWING CAREFULLY AND FILL IN THE GAPS
-
-I hereby declare that all the work that was required to 
-solve the following problem including designing the algorithms
-and writing the code below, is solely my own and that I received
-no help in creating this solution and I have not discussed my solution 
-with anybody. I affirm that I have read and understood
-the Senate Policy on Academic honesty at 
-https://secretariat-policies.info.yorku.ca/policies/academic-honesty-senate-policy-on/
-and I am well aware of the seriousness of the matter and the penalties that I will face as a 
-result of committing plagiarism in this assignment.
-
-BY FILLING THE GAPS,YOU ARE SIGNING THE ABOVE STATEMENTS.
-
-Full Name: Jaideep Kular	
-Student Number: 217349549
-Course Section: A
- */
 
 
 /**
